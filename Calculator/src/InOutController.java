c29a577874b68d8324cc849d946a848c334419c4
Alexander Tretyackov <tretyackov@biogenom.ru>
2022-03-31 16:26:45 +0500

    feat: Валидация выражения

--- Calculator/src/InOutController.java
+++ Calculator/src/InOutController.java
@@ -35,14 +35,75 @@ public class InOutController {
                 System.out.print("Error! Division by zero!");
             }
         }
-        else
-            System.out.print("Error! Invalid expression!");
     }
 
     //валидация введенного выражения
     private static boolean isValid(String expression) {
-        //заглушка
-        //должна быть проверка валидации
+        char[] chArray = expression.toCharArray();
+        int previous = 0;
+        int brace = 0;
+        for (int i = 0; i < expression.length(); i++){
+            char symbol = chArray[i];
+            switch (symbol){
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    if(previous < 3){
+                        System.out.println("Знак может стоять только после числа или ')'.");
+                        return false;
+                    }
+                    previous = 1;
+                    break;
+                case '(':
+                    if (previous == 3){
+                        System.out.println("'(' не может стоять после ')'.");
+                        return false;
+                    }
+                    brace = brace + 1;
+                    previous = 2;
+                    break;
+                case ')':
+                    if (previous != 4){
+                        System.out.println("')' может стоять только после числа.");
+                        return false;
+                    }
+                    if (brace == 0){
+                        System.out.println("Для использования закрывающей скобки, сначла должна быть напечатана парная ей открывающая скобка.");
+                        return false;
+                    }
+                    brace = brace - 1;
+                    previous = 3;
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                    if(previous == 3){
+                        System.out.println("Число не может стоять сразу после закрывающей скобки.");
+                        return false;
+                    }
+                    previous = 4;
+                    break;
+                default:
+                    System.out.println("'"+symbol +  "' нелья использовать в выражении.");
+                    return false;
+            }
+        }
+        if (brace != 0 ){
+            System.out.println("Есть незакрытые скобки");
+            return false;
+        }
+        if (previous == 1){
+            System.out.println("Выражение не может заканчиваться знаком");
+            return false;
+        }
         return true;
     }
 }
